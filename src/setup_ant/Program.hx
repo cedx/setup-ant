ca40c7ded748a3b8d634bdc5f47adc40beebef98
Cédric Belin <cedric@belin.io>
2023-07-29 22:11:44 +0200

    Improve the install message

--- src/setup_ant/Program.hx
+++ src/setup_ant/Program.hx
@@ -5,14 +5,19 @@ import tink.semver.Constraint;
 
 /** Application entry point. **/
 function main() {
+	final optionalTasks = Core.getBooleanInput("optional-tasks");
 	final version = Core.getInput("version");
+
 	switch Constraint.parse(version.length == 0 || version == "latest" ? "*" : version) {
 		case Failure(_): Core.setFailed("Invalid version constraint.");
 		case Success(constraint): switch Release.find(constraint) {
 			case None: Core.setFailed("No release matching the version constraint.");
-			case Some(release): new Setup(release, Core.getBooleanInput("optional-tasks")).install().handle(outcome -> switch outcome {
-				case Failure(error): Core.setFailed(error.message);
-				case Success(path): Core.info('Apache Ant ${release.version} successfully installed in "${path}".');
+			case Some(release): new Setup(release, optionalTasks).install().handle(outcome -> switch outcome {
+				case Failure(error):
+					Core.setFailed(error.message);
+				case Success(path):
+					final installed = optionalTasks ? "installed with optional tasks" : "installed";
+					Core.info('Apache Ant ${release.version} successfully $installed in "$path".');
 			});
 		}
 	}

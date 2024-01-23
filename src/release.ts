a3315d178b2b7ae82ee3e204bb580b45d43fb01e
Cédric Belin <cedric@belin.io>
2024-01-23 22:16:20 +0100

    Port the `Release` class to TS

--- src/release.ts
+++ src/release.ts
@@ -1,6 +1,70 @@
+import semver from "semver";
+import releases from "../res/releases.json" with {type: "json"};
+
 /**
  * Represents a GitHub release.
  */
 export class Release {
-	
+
+	/**
+	 * The base URL of the releases.
+	 */
+	static readonly #baseUrl = new URL("https://dlcdn.apache.org/ant/binaries/");
+
+	/**
+	 * The list of all releases.
+	 */
+	static readonly #data = releases.map(release => new this(release.version));
+
+	/**
+	 * The version number.
+	 */
+	readonly version: string;
+
+	/**
+	 * Creates a new release.
+	 * @param version The version number.
+	 */
+	constructor(version: string) {
+		this.version = version;
+	}
+
+	/**
+	 * The latest release.
+	 */
+	static get latest(): Release {
+		return this.#data[0];
+	}
+
+	/**
+	 * Value indicating whether this release exists.
+	 */
+	get exists(): boolean {
+		return Release.#data.some(release => release.version == this.version);
+	}
+
+	/**
+	 * The download URL.
+	 */
+	get url(): URL {
+		return new URL(`apache-ant-${this.version}-bin.zip`, Release.#baseUrl);
+	}
+
+	/**
+	 * Finds a release that matches the specified version constraint.
+	 * @param constraint The version constraint.
+	 * @returns The release corresponding to the specified constraint.
+	 */
+	static find(constraint: string): Release|null {
+		return this.#data.find(release => semver.satisfies(release.version, constraint)) ?? null;
+	}
+
+	/**
+	 * Gets the release corresponding to the specified version.
+	 * @param version The version number of a release.
+	 * @returns The release corresponding to the specified version.
+	 */
+	static get(version: string): Release|null {
+		return this.#data.find(release => release.version == version) ?? null;
+	}
 }
---
+++ test/release_test.js
@@ -0,0 +1,9 @@
+import assert from "node:assert/strict";
+import {describe, it} from "node:test";
+import {Release} from "#setup_ant";
+
+/**
+ * Tests the features of the {@link Release} class.
+ */
+describe("Release", () => {
+});

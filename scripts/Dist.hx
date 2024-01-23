b094d5400a55e316cc5852c6f395d1f82cd31e6b
Cédric Belin <cedric@belin.io>
2024-01-23 17:03:13 +0100

    Update the configuration

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -1,6 +1,6 @@
 /** Packages the project. **/
 function main() {
-	final file = "bin/setup_ant.js";
+	final file = "bin/setup_ant.mjs";
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
 	Sys.command('npx esbuild --allow-overwrite --bundle --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
 	Sys.command('git update-index --chmod=+x $file');

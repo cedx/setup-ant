b6ad2503f1ab166b99e70364f03d226ddb895f75
Cédric Belin <cedric@belin.io>
2024-11-03 00:56:11 +0100

    Port the build system to Cake

--- scripts/Dist.hx
+++
@@ -1,6 +0,0 @@
-/** Packages the project. **/
-function main() {
-	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
-	Sys.command("npx rollup --config=etc/rollup.js");
-	Sys.command("git update-index --chmod=+x bin/setup_ant.js");
-}

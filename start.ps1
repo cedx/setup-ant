230a6084f506f36c4fb96942dda6d0ed6eb0771a
Cédric Belin <cedx@outlook.com>
2025-10-14 19:40:48 +0200

    Optimization?

--- start.ps1
+++ start.ps1
@@ -1,7 +1,15 @@
-echo "args"
+#!/usr/bin/env pwsh
+# using module ./src/Release.psm1
+# using module ./src/Setup.psm1
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+echo "- args"
 echo $args
-echo "env:SETUP_ANT_OPTIONAL_TASKS"
+echo "- env:SETUP_ANT_OPTIONAL_TASKS"
 echo $ENV:SETUP_ANT_OPTIONAL_TASKS
-echo "env:SETUP_ANT_VERSION"
+echo "- env:SETUP_ANT_VERSION"
 echo $ENV:SETUP_ANT_VERSION
-echo "OK !"
+echo - "OK !"

131284328fc02cb35ab02b6bfac570cc02ba9d7e
Cédric Belin <cedx@outlook.com>
2025-10-14 16:51:54 +0200

    Try to fix the CI

--- SetupAnt.ps1
+++ SetupAnt.ps1
@@ -6,7 +6,10 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
+echo "args"
 echo $args
+echo "env:SETUP_ANT_OPTIONAL_TASKS"
 echo $ENV:SETUP_ANT_OPTIONAL_TASKS
+echo "env:SETUP_ANT_VERSION"
 echo $ENV:SETUP_ANT_VERSION
 echo "OK !"

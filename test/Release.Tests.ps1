5b4eeaf7373306e9e88796a33989023834c1161d
Cédric Belin <cedx@outlook.com>
2025-10-18 13:01:29 +0200

    Use `Context` blocks

--- test/Release.Tests.ps1
+++ test/Release.Tests.ps1
@@ -17,19 +17,19 @@ Describe "Release" {
 		$nonExistingRelease = [Release] "666.6.6"
 	}
 
-	Describe "Exists" {
+	Context "Exists" {
 		It "should return `$true if the release exists" { $existingRelease.Exists() | Should -BeTrue }
 		It "should return `$false if the release does not exist" { $nonExistingRelease.Exists() | Should -BeFalse }
 	}
 
-	Describe "Url" {
+	Context "Url" {
 		It "should return the URL of the Ant archive" {
 			$existingRelease.Url() | Should -BeExactly "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.15-bin.zip"
 			$nonExistingRelease.Url() | Should -BeExactly "https://archive.apache.org/dist/ant/binaries/apache-ant-666.6.6-bin.zip"
 		}
 	}
 
-	Describe "Find" {
+	Context "Find" {
 		It "should return `$null if no release matches the version constraint" {
 			[Release]::Find("666.6.6") | Should -Be $null
 		}
@@ -51,12 +51,12 @@ Describe "Release" {
 		}
 	}
 
-	Describe "Get" {
+	Context "Get" {
 		It "should return `$null if no release matches to the version number" { [Release]::Get("666.6.6") | Should -Be $null }
 		It "should return the release corresponding to the version number if it exists" { [Release]::Get("1.8.2")?.Version | Should -Be "1.8.2" }
 	}
 
-	Describe "Latest" {
+	Context "Latest" {
 		It "should exist" { $latestRelease | Should -Not -Be $null }
 	}
 }

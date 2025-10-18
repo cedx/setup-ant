080462beaab22de2ae570111973ff49e568f26ac
Cédric Belin <cedx@outlook.com>
2025-10-18 19:05:30 +0200

    Update the "Test" script [skip ci]

--- tool/Test.ps1
+++ tool/Test.ps1
@@ -2,5 +2,12 @@
 pwsh -Command {
 	Import-Module Pester
 	Invoke-Pester test
+
+	if (Get-Module BurntToast -ListAvailable) {
+		Import-Module BurntToast
+		$message = $LASTEXITCODE -eq 0 ? "were successfully completed" : "failed with $LASTEXITCODE $($LASTEXITCODE -gt 1 ? "errors" : "error")"
+		New-BurntToastNotification -AppLogo Program.ico -Text "Setup Ant", "The tests $message." -UniqueIdentifier Belin.SetupAnt
+	}
+
 	exit $LASTEXITCODE
 }

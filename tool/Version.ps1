d19076300598abbc212a65cb421faca027e9860d
Cédric Belin <cedx@outlook.com>
2025-11-13 17:23:42 +0100

    Remove the "Version" script [skip ci]

--- tool/Version.ps1
+++
@@ -1,3 +0,0 @@
-"Updating the version number in the sources..."
-$version = (Import-PowerShellDataFile "SetupAnt.psd1").ModuleVersion
-(Get-Content "ReadMe.md") -replace "module/v\d+(\.\d+){2}", "module/v$version" | Out-File "ReadMe.md"

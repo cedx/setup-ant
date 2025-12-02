2426558845d8b81e0f9390917577c19ab67e2ee4
Cédric Belin <cedx@outlook.com>
2025-12-02 21:51:36 +0100

    Port the `Install-Release` cmdlet

--- src/Cmdlets/Install-Release.cs
+++ src/Cmdlets/Install-Release.cs
@@ -3,6 +3,33 @@ namespace Belin.SetupAnt.Cmdlets;
 /// <summary>
 /// Installs Apache Ant, after downloading it.
 /// </summary>
-public class InstallReleaseCommand: Cmdlet {
+[Cmdlet(VerbsLifecycle.Install, "Release")]
+[OutputType(typeof(string))]
+public class InstallReleaseCommand: PSCmdlet {
 
+	/// <summary>
+	/// The instance of the release to be installed.
+	/// </summary>
+	[Parameter(Mandatory = true, ParameterSetName = "InputObject", ValueFromPipeline = true)]
+	public required Release InputObject { get; set; }
+
+	/// <summary>
+	/// Value indicating whether to fetch the Ant optional tasks.
+	/// </summary>
+	[Parameter]
+	public SwitchParameter OptionalTasks { get; set; }
+
+	/// <summary>
+	/// The version number of the release to be installed.
+	/// </summary>
+	[Parameter(Mandatory = true, ParameterSetName = "Version", Position = 0, ValueFromPipeline = true)]
+	public required Version Version { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() {
+		var release = ParameterSetName == "InputObject" ? InputObject : new Release(Version);
+		// TODO WriteObject(new Setup(release).Install(OptionalTasks));
+	}
 }

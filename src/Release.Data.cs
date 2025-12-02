2e769f4bea8ceb303509701eb90cf1d8ef003117
Cédric Belin <cedx@outlook.com>
2025-12-02 11:13:27 +0100

    Port the release data

---
+++ src/Release.Data.cs
@@ -0,0 +1,61 @@
+namespace Belin.SetupAnt;
+
+/// <summary>
+/// Represents an Apache Ant release.
+/// </summary>
+public partial class Release {
+
+	/// <summary>
+	/// The list of all releases.
+	/// </summary>
+	private static readonly Release[] data = [
+		new Release("1.10.15"),
+		new Release("1.10.14"),
+		new Release("1.10.13"),
+		new Release("1.10.12"),
+		new Release("1.10.11"),
+		new Release("1.10.10"),
+		new Release("1.10.9"),
+		new Release("1.10.8"),
+		new Release("1.10.7"),
+		new Release("1.10.6"),
+		new Release("1.10.5"),
+		new Release("1.10.4"),
+		new Release("1.10.3"),
+		new Release("1.10.2"),
+		new Release("1.10.1"),
+		new Release("1.10.0"),
+		new Release("1.9.16"),
+		new Release("1.9.15"),
+		new Release("1.9.14"),
+		new Release("1.9.13"),
+		new Release("1.9.12"),
+		new Release("1.9.11"),
+		new Release("1.9.10"),
+		new Release("1.9.9"),
+		new Release("1.9.8"),
+		new Release("1.9.7"),
+		new Release("1.9.6"),
+		new Release("1.9.5"),
+		new Release("1.9.4"),
+		new Release("1.9.3"),
+		new Release("1.9.2"),
+		new Release("1.9.1"),
+		new Release("1.9.0"),
+		new Release("1.8.4"),
+		new Release("1.8.3"),
+		new Release("1.8.2"),
+		new Release("1.8.1"),
+		new Release("1.8.0"),
+		new Release("1.7.1"),
+		new Release("1.7.0"),
+		new Release("1.6.5"),
+		new Release("1.6.4"),
+		new Release("1.6.3"),
+		new Release("1.6.2"),
+		new Release("1.6.1"),
+		new Release("1.6.0"),
+		new Release("1.5.4"),
+		new Release("1.5.2")
+	];
+}

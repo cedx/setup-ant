6280c38a3e38946219eade31f3dbc44b9b7c6e75
Cédric Belin <cedx@outlook.com>
2025-12-02 11:01:30 +0100

    Add the cmdlet skeletons

--- src/Cmdlets/Find-Release.cs
+++ src/Cmdlets/Find-Release.cs
@@ -1,1 +1,8 @@
 namespace Belin.SetupAnt.Cmdlets;
+
+/// <summary>
+/// Finds a release that matches the specified version constraint.
+/// </summary>
+public class FindReleaseCommand: Cmdlet {
+
+}
--- src/Cmdlets/Test-Release.cs
+++ src/Cmdlets/Test-Release.cs
@@ -1,1 +1,8 @@
 namespace Belin.SetupAnt.Cmdlets;
+
+/// <summary>
+/// Gets a value indicating whether a release with the specified version exists.
+/// </summary>
+public class TestReleaseCommand: Cmdlet {
+
+}

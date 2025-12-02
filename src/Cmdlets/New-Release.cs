58fc6ca0a648bc5b65b0330e2602908e8848730d
Cédric Belin <cedx@outlook.com>
2025-12-02 21:18:45 +0100

    Port the `New-Release` cmdlet

--- src/Cmdlets/New-Release.cs
+++ src/Cmdlets/New-Release.cs
@@ -3,6 +3,18 @@ namespace Belin.SetupAnt.Cmdlets;
 /// <summary>
 /// Creates a new release.
 /// </summary>
+[Cmdlet(VerbsCommon.New, "Release")]
+[OutputType(typeof(Release))]
 public class NewReleaseCommand: Cmdlet {
 
+	/// <summary>
+	/// The version number.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+	public required Version Version { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() => WriteObject(new Release(Version));
 }

bec78da14b7ce3036e3301d75a767adf7ac99317
Cédric Belin <cedx@outlook.com>
2025-12-02 22:35:00 +0100

    Add the skeleton of the `Setup` class

---
+++ test/Setup.Tests.cs
@@ -0,0 +1,17 @@
+namespace Belin.SetupAnt;
+
+/// <summary>
+/// Tests the features of the <see cref="Setup"/> class.
+/// </summary>
+/// <param name="testContext">The test context.</param>
+[TestClass]
+public sealed class SetupTests(TestContext testContext) {
+
+	[TestMethod]
+	public void Download() {
+	}
+
+	[TestMethod]
+	public void Install() {
+	}
+}

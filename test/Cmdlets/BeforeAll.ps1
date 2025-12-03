317700331a1425c661d54ea6c28b77fd2938f12e
Cédric Belin <cedx@outlook.com>
2025-12-03 10:09:33 +0100

    Fix the warnings of the static analyzer

--- test/Cmdlets/BeforeAll.ps1
+++ test/Cmdlets/BeforeAll.ps1
@@ -1,5 +1,11 @@
+using namespace System.Diagnostics.CodeAnalysis
 Import-Module "$PSScriptRoot/../../SetupAnt.psd1"
 
+[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "")]
 $existingRelease = New-AntRelease "1.10.15"
+
+[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "")]
 $latestRelease = Get-AntRelease "Latest"
+
+[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "")]
 $nonExistingRelease = New-AntRelease "666.6.6"

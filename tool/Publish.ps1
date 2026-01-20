3b05e6515c1ad8f1291d55c7ab704d79350935c8
Cédric Belin <cedx@outlook.com>
2026-01-20 21:20:10 +0100

    Code formatting [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -5,6 +5,6 @@ else {
 }
 
 "Publishing the package..."
-$version = (Import-PowerShellDataFile "SetupAnt.psd1").ModuleVersion
+$version = Import-PowerShellDataFile SetupAnt.psd1 | Select-Object -ExpandProperty ModuleVersion
 git tag "v$version"
 git push origin "v$version"

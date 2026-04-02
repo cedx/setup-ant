f532750bbfa98378ad80b50ed525c7d28824fd0f
Cédric Belin <cedx@outlook.com>
2026-04-02 12:03:45 +0200

    Code optimization

--- Start.ps1
+++ Start.ps1
@@ -6,11 +6,8 @@ Set-StrictMode -Version Latest
 Import-Module "$PSScriptRoot/SetupAnt.psd1"
 if (-not (Test-Path Env:SETUP_ANT_VERSION)) { $Env:SETUP_ANT_VERSION = "Latest" }
 
-$release = Find-AntRelease $Env:SETUP_ANT_VERSION
-if (-not $release) { throw "No release matches the specified version constraint." }
-
 $optionalTasks = $Env:SETUP_ANT_OPTIONAL_TASKS -eq "true"
-$path = Install-AntRelease -InputObject $release -OptionalTasks
+$path = Install-AntRelease $Env:SETUP_ANT_VERSION -OptionalTasks:$optionalTasks
 
 $installed = $optionalTasks ? "installed with optional tasks" : "installed"
 "Apache Ant $($release.Version) successfully $installed in ""$path""."
